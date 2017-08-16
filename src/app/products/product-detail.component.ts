00ce163ba4ce9b072941a8adfc042389362f43c3
Sebastian Restrepo <sebastian_restrepo@ultimatesoftware.com>
2017-08-16 15:03:28 -0400

    Additional routing techinques, mainly using Guards as services.

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
 
 @Component({
@@ -10,9 +11,18 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail'
   product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) { }
 
   ngOnInit() {
+    // This is how you get the url parameter 'id', there are alternatives way that might be worth investigating
+    let id = +this._route.snapshot.paramMap.get('id');
+    // The let is used to create a block variable, not sure what that means but it has something to do with scope
+    // The + is a shortcut to cast a string to a number
+    this.pageTitle += `: ${id}`;
   }
 
+  onBack(): void {
+    this._router.navigate(['/products'])
+  }
 }
---
+++ src/app/products/product-guard.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, Router } from "@angular/router";
+
+@Injectable()
+export class ProductGuardService implements CanActivate{
+
+  constructor(private _router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    let id = +route.url[1].path; //url is made up of two segments; 1:products, 2:10 because the url is products/10
+    if( isNaN(id) || id < 1 ) {
+      alert("Invalid Product Id");
+      this._router.navigate(['/products']);
+      return false;
+    }
+    return true;
+  }
+
+  // This shows the different parameters that can be passed in by default and the different possible return values.
+  // These seem more important when you need authentication and things of that nature
+  // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+  //   throw new Error("Method not implemented.");
+  // }
+}
